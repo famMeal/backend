e7ff117ecaa30ed8d9e68b207b6de3f5f9f52a64
Kristine McBride <kristine.mcbride@fullscript.com>
2024-04-07 19:34:11 -0400

    update all order status mutation

--- app/graphql/mutations/order/update_all_orders_status.rb
+++ app/graphql/mutations/order/update_all_orders_status.rb
@@ -1,8 +1,8 @@
 
 module Mutations::Order
   class UpdateAllOrdersStatus < Mutations::BaseMutation
-    argument :from_status, String, required: true
-    argument :to_status, String, required: true
+    argument :from_status, Types::FilterObjects::OrderStatusField, required: true
+    argument :to_status, Types::FilterObjects::OrderStatusField, required: true
     argument :restaurant_id, ID, required: true
 
     field :errors, [String], null: false
@@ -10,8 +10,17 @@ module Mutations::Order
 
     def resolve(**args)
       orders = Order.by_restaurant_and_status(args[:restaurant_id], args[:from_status])
-     
-      orders.update_all(status: args[:to_status])
+      previous_status = args[:from_status]
+      
+      new_status = if args[:to_status] == "completed" && previous_status == "picked_up"
+        "completed_restaurant"
+      elsif args[:to_status] == "completed"
+        "completed_client"
+      else
+        args[:to_status]
+      end
+
+      orders.update_all(status: new_status)
     
       orders_result = Order.by_restaurant_and_status(args[:restaurant_id], args[:to_status])
       { orders: orders_result, errors: [] }
