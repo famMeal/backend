f878509342519a738844abccaebc4e45112ceefd
Kristine McBride <kristine.mcbride@fullscript.com>
2024-04-10 21:01:41 -0400

    update order with tip

--- app/graphql/mutations/order/update_order.rb
+++ app/graphql/mutations/order/update_order.rb
@@ -6,22 +6,31 @@ module Mutations::Order
     argument :quantity, Integer, required: false
     argument :pickup_start_time, String, required: false
     argument :pickup_end_time, String, required: false
+    argument :tip_amount, Float, required: false
+    argument :tip_percentage, Integer, required: false
 
     field :errors, [String], null: false
     field :order, Types::OrderType, null: true
 
     def resolve(**args)
       @order = Order.find(args[:order_id])
-      
+      @subtotal = Meal.find(order.meal_id).price * args[:quantity]
+
       update_status(args[:status]) if args[:status]
+      
       order.pickup_start_time = args[:pickup_start_time] if args[:pickup_start_time]
       order.pickup_end_time = args[:pickup_end_time] if args[:pickup_end_time]
 
+      total_without_tip = subtotal * 1.13
+      tip_amount = args[:tip_amount] || total_without_tip * (args[:tip_percentage].to_d / 100.00)
+ 
       if args[:quantity]
         order.assign_attributes(
           quantity: args[:quantity],
-          subtotal: calculate_subtotal(args[:quantity], order.meal_id),
-          total: subtotal * 1.13
+          subtotal: subtotal,
+          total: total_without_tip + tip_amount,
+          tip_amount: tip_amount,
+          tip_percentage: args[:tip_percentage] || nil
         )  
       end
 
@@ -30,10 +39,6 @@ module Mutations::Order
       { order: order, errors: [] }
     rescue StandardError => e
       { errors: [e.message] }
-    end
-
-    def calculate_subtotal(quantity, meal_id)
-      @subtotal ||= Meal.find(meal_id).price * quantity
     end
 
     def update_status(status)
