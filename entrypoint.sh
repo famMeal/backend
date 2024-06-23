b048e6ea867a20d3862f7747a7509c3a8f36b14e
Kristine McBride <kristine.mcbride@fullscript.com>
2024-06-23 15:34:07 -0400

    fix entyrpoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,15 @@ set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /backend/tmp/pids/server.pid
-
+# Check if the database exists
+if bundle exec rails db:version; then
+  echo "Database already exists. Running migrations..."
+  bundle exec rails db:migrate
+else
+  echo "Database does not exist. Creating and migrating..."
+  bundle exec rails db:create
+  bundle exec rails db:migrate
+  bundle exec rails db:seed
+fi
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
