8024a2e73778f084b14626c3981c42fea6b1e61e
Kristine McBride <kristine.mcbride@fullscript.com>
2024-07-21 14:52:25 -0400

    fix signup mutation

--- app/graphql/mutations/auth/sign_up.rb
+++ app/graphql/mutations/auth/sign_up.rb
@@ -24,17 +24,13 @@ module Mutations::Auth
 
     def resolve(confirm_url: nil, **attrs)
       return raise_user_error_list("Passwords do not match", errors: []) if attrs[:password] != attrs[:password_confirmation]
-      
+
       ActiveRecord::Base.transaction do
         resource = build_resource(attrs.except(:restaurant_name, :certificate_number, :longitude, :latitude, :address_line_1, :address_line_2, :city, :province, :postal_code).merge(provider: provider, confirmation_token: rand(100000...999999).to_s))
-  
-        resource.save!
-
-        yield resource if block_given?
 
         if attrs[:restaurant_name].present?
-           Restaurant.create!(
-            name: attrs[:restaurant_name], 
+          restaurant = Restaurant.create!(
+            name: attrs[:restaurant_name],
             certificate_number: attrs[:certificate_number],
             latitude: attrs[:latitude],
             longitude: attrs[:longitude],
@@ -45,8 +41,15 @@ module Mutations::Auth
             postal_code: attrs[:postal_code],
             country: "canada"
             )
+
+          resource.restaurant = restaurant
+          resource.is_store_owner = true
         end
-        
+
+        resource.save!
+
+        yield resource if block_given?
+
         unless resource.confirmed?
           resource.send_confirmation_instructions(
             confirmation_code:  resource.confirmation_token,
@@ -58,7 +61,7 @@ module Mutations::Auth
 
         response_payload[:credentials] = generate_auth_headers(resource) if resource.active_for_authentication?
 
-        response_payload 
+        response_payload
       end
     rescue => e
       return raise_user_error_list(e.message, errors: [])
